4a610c857b59145f5b45b8f18ab3492c96b43a5f
Maties <maruan@floridauniversitaria.es>
2016-11-19 17:35:32 +0100

    primer commit

---
+++ src/SumaDosEnteros.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumaDosEnteros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		x=5+2;
+		System.out.println(x);
+		
+			
+	}
+
+}
